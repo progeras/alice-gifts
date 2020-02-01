13ff6742404a0ac02ff277d0bd95c1cccd14ef85
Asan Shamratov <ashamratov@eccentex.com>
2020-02-01 19:09:51 +0300

    gifts

--- src/commands/router/questions/giftsHelper.js
+++ src/commands/router/questions/giftsHelper.js
@@ -1,5 +1,29 @@
 const gifts = require('./../../../gifts');
+const utils = require('./../../utils');
 
 module.exports.getGifts = function (ctx, count) {
-    return gifts;
+    let current = utils.getSelected(ctx),
+        filtered = gifts;
+    if(current.age){
+        filtered = filtered.filter((c) => {
+            return current.age >= c.min_age && current.age <= c.max_age;
+        })
+    }
+
+    if(current.price){
+        filtered = filtered.filter((c) => {
+            return c.price <= current.price;
+        })
+    }
+
+    if(current.sex){
+        filtered = filtered.filter((c) => {
+            return c.sex.includes(current.sex);
+        })
+    }
+
+    return filtered.sort((a, b) => {
+        return a.price - b.price
+    }).slice(0, count);
+
 }
--- src/commands/router/questions/price.js
+++ src/commands/router/questions/price.js
@@ -4,12 +4,24 @@ const giftsHelper = require('./giftsHelper');
 const _ = require('lodash');
 
 module.exports = function (ctx) {
-    utils.setSelected(ctx, {price: ctx.message});
+    const matches = /.*\d+.*/
+    const { message } = ctx
+    if(!matches.test(message)){
+        const replyes = [
+            "Я вообще то спросила цену... Назовите плиз цену в рублях'",
+            "Это не цена, назовите в рублях, например '1000' или '1500'"
+        ];
+        return alice.Reply.text(_.sample(replyes))
+    }
+    let price = +message.match(/\d+/)
+
+    utils.setSelected(ctx, {price: price});
+
     let g = giftsHelper.getGifts(ctx, 3);
     let answer = g.map((c) => {
         return `${c.title} (${c.price}р)`;
     }).join('\n');
 
-    utils.setScene(ctx, 'gifts-list');
+    utils.setScene(ctx);
     return alice.Reply.text(answer);
 }
