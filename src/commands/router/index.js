a101408cad4300051fb742a517805f8ce7d8b665
Asan Shamratov <ashamratov@eccentex.com>
2020-02-01 16:26:27 +0300

    refactoring

--- src/commands/router/index.js
+++ src/commands/router/index.js
@@ -1,10 +1,16 @@
 const alice = require('yandex-dialogs-sdk');
 const _ = require('lodash');
 
+const age = require('./questions/age');
+
 const matcher = /.*/
 
 exports.matcher = matcher;
 exports.handler = function (ctx) {
+    const currState = ctx.session.get('scene');
+    if(currState === 'age'){
+        return age(ctx);
+    }
 
     return alice.Reply.text(`
         Сюда впилить роутинг в соответствие со стейтом
